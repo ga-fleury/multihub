78bced85318ab9974880cb8e64195a6e6e5375af
Gabriel Fleury <gabrielfleury95@gmail.com>
2023-08-02 17:50:52 -0300

    api call

--- script.js
+++ script.js
@@ -34,90 +34,6 @@ let FINAL_FORM_DATA = {
     city: ''
 }
 
-let API_POST_REQUEST_BODY = {
-    "fields": [
-        {
-            "objectTypeId": "0-1",
-            "name": "multi_rental_date_start",
-            "value": FINAL_FORM_DATA.multi_rental_date_start
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "email",
-            "value": FINAL_FORM_DATA.email
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "multi_rental_date_to",
-            "value": FINAL_FORM_DATA.multi_rental_date_to
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "bulk_rental_vehicle_type",
-            "value": FINAL_FORM_DATA.bulk_rental_vehicle_type
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "bulk_rental_vehicle_subtype",
-            "value": FINAL_FORM_DATA.bulk_rental_vehicle_subtype
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "daily_rate",
-            "value": FINAL_FORM_DATA.daily_rate
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "vehicle_units",
-            "value": FINAL_FORM_DATA.vehicle_units
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "bulk_rental_location",
-            "value": FINAL_FORM_DATA.bulk_rental_location
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "bulk_rental_radius_miles",
-            "value": FINAL_FORM_DATA.bulk_rental_radius_miles
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "bulk_rental_comment",
-            "value": FINAL_FORM_DATA.bulk_rental_comment
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "full_name",
-            "value": FINAL_FORM_DATA.full_name
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "company",
-            "value": FINAL_FORM_DATA.company
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "phone",
-            "value": FINAL_FORM_DATA.phone
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "dot_number",
-            "value": FINAL_FORM_DATA.dot_number
-        },
-        {
-            "objectTypeId": "0-1",
-            "name": "city",
-            "value": FINAL_FORM_DATA.city
-        }
-    ],
-    "context": {
-        "pageUri": "www.coop.com/multi-vehicle-request",
-        "pageName": "Bulk Rental Form"
-    }
-}
-
 /**
  * An object with arrays for the different vehicle types,
  * used to populate the dropdown once the user selects a Vehicle Type
@@ -432,7 +348,6 @@ function getCookies() {
     var hubspotCookie = document.cookie.match(new RegExp('(^| )' + 'hubspotutk' + '=([^;]+)'));
     if (hubspotCookie) {
         hubspotCookieField.val(`${hubspotCookie[2]}`);
-        API_POST_REQUEST_BODY.context.hutk = hubspotCookie[2];
     }
     else {
         console.log('--something went wrong---');
@@ -781,6 +696,92 @@ async function formSubmissionCall() {
     let requestHeader = {
         "Authorization": authToken,
         "content-type": "application/json"
+    }
+    let API_POST_REQUEST_BODY = {
+        "fields": [
+            {
+                "objectTypeId": "0-1",
+                "name": "multi_rental_date_start",
+                "value": FINAL_FORM_DATA.multi_rental_date_start
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "email",
+                "value": FINAL_FORM_DATA.email
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "multi_rental_date_to",
+                "value": FINAL_FORM_DATA.multi_rental_date_to
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "bulk_rental_vehicle_type",
+                "value": FINAL_FORM_DATA.bulk_rental_vehicle_type
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "bulk_rental_vehicle_subtype",
+                "value": FINAL_FORM_DATA.bulk_rental_vehicle_subtype
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "daily_rate",
+                "value": FINAL_FORM_DATA.daily_rate
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "vehicle_units",
+                "value": FINAL_FORM_DATA.vehicle_units
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "bulk_rental_location",
+                "value": FINAL_FORM_DATA.bulk_rental_location
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "bulk_rental_radius_miles",
+                "value": FINAL_FORM_DATA.bulk_rental_radius_miles
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "bulk_rental_comment",
+                "value": FINAL_FORM_DATA.bulk_rental_comment
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "full_name",
+                "value": FINAL_FORM_DATA.full_name
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "company",
+                "value": FINAL_FORM_DATA.company
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "phone",
+                "value": FINAL_FORM_DATA.phone
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "dot_number",
+                "value": FINAL_FORM_DATA.dot_number
+            },
+            {
+                "objectTypeId": "0-1",
+                "name": "city",
+                "value": FINAL_FORM_DATA.city
+            }
+        ],
+        "context": {
+            "pageUri": "www.coop.com/multi-vehicle-request",
+            "pageName": "Bulk Rental Form"
+        }
+    }
+    if (hubspotCookieField.val()) {
+        API_POST_REQUEST_BODY.context.hutk = hubspotCookieField.val();
     }
     try {
         const response = await fetch(requestURL, {
